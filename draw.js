cdba701b8eb3cb292f3ad89ee1407f7c2c8957c7
Awet Fessha <awetkifle94@gmail.com>
2019-04-18 12:03:01 +0100

    add more event listners

--- draw.js
+++ draw.js
@@ -2,6 +2,10 @@ var redButton = document.getElementById("btnRed");
 var greenButton = document.getElementById("btnGreen");
 var yellowButton = document.getElementById("btnYellow");
 var purpleButton = document.getElementById("btnPurple");
+var smallButton = document.getElementById("btnSmall");
+var normalButton = document.getElementById("btnNormal");
+var largeButton = document.getElementById("btnLarge");
+var hugeButton = document.getElementById("btnHuge");
 
 redButton.addEventListener("click", function changeColor() {
   alert("Red");
@@ -14,4 +18,18 @@ yellowButton.addEventListener("click", function displayMessage() {
 });
 purpleButton.addEventListener("click", function displayMessage() {
   alert("you have clicked the Purple button");
+});
+
+//For selcting size
+smallButton.addEventListener("click", function displayMessage() {
+  alert("you have clicked the Small button");
+});
+normalButton.addEventListener("click", function displayMessage() {
+  alert("you have clicked the Normal button");
+});
+largeButton.addEventListener("click", function displayMessage() {
+  alert("you have clicked the Large button");
+});
+hugeButton.addEventListener("click", function displayMessage() {
+  alert("you have clicked the Huge button");
 });
