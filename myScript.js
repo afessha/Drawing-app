65e287366e17d02aceb289bac2deaa5b5febe054
Awet Fessha <awetkifle94@gmail.com>
2019-04-25 11:53:56 +0100

    remove node and create new js file

---
+++ myScript.js
@@ -0,0 +1,74 @@
+$("document").ready(function() {
+  var canvas = document.getElementById("draw");
+  var paint, context, mouseX, mouseY;
+  var clickX = new Array();
+  var clickY = new Array();
+  var clickDrag = new Array();
+  var width = 600;
+  var height = 400;
+  //Listening to mouse down event
+  if (canvas.addEventListener) {
+    canvas.addEventListener("mousedown", function(e) {
+      paint = true;
+      var mouseX = e.pageX - this.offsetLeft;
+      var mouseY = e.pageY - this.offsetTop;
+      addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+      reDraw();
+    });
+  }
+
+  //Listening to the mouse move event
+  if (canvas) {
+    if (canvas.addEventListener) {
+      canvas.addEventListener("mousemove", function(e) {
+        if (paint) {
+          addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+          reDraw();
+        }
+      });
+    }
+  }
+
+  //Listening to mouse up event
+  if (canvas) {
+    if (canvas.addEventListener) {
+      canvas.addEventListener("mouseup", function(e) {
+        paint = false;
+      });
+    }
+  }
+  //Listening to mouse leave event
+  if (canvas) {
+    if (canvas.addEventListener) {
+      canvas.addEventListener("mouseleave", function(e) {
+        paint = false;
+      });
+    }
+  }
+
+  //Defining the function called addClick
+  function addClick(x, y, dragging) {
+    clickX.push(x);
+    clickY.push(y);
+    clickDrag.push(dragging);
+  }
+  //Defining the function called reDraw
+  function reDraw() {
+    var context = canvas.getContext("2d");
+    context.clearRect(0, 0, width, height);
+    context.strokeStyle = "#df4b26";
+    context.lineJoin = "round";
+    context.lineWidth = 5;
+    for (var i = 0; i < clickX.length; i++) {
+      context.beginPath();
+      if (clickDrag[i] && i) {
+        context.moveTo(clickX[i - 1], clickY[i - 1]);
+      } else {
+        context.moveTo(clickX[i] - 1, clickY[i]);
+      }
+      context.lineTo(clickX[i], clickY[i]);
+      context.closePath();
+      context.stroke();
+    }
+  }
+});
